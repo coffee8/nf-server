c486504a3404b88b8303e2b653c15b41925ee4be
coffee8 <heavenlywork@outlook.com>
2023-08-25 17:54:32 +0500

    fix: delete redundant comment

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ server.post('/login', (req, res) => {
 });
 
 // checking whether user is authorized
-// eslint-disable-next-line consistent-return
 server.use((req, res, next) => {
     if (!req.headers.authorization) {
         return res.status(403).json({ message: 'AUTH ERROR' });
